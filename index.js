42b531bd3ca80ec5a155a7a723567aa2ee40b9be
aakashrajput <aakashsingh1999@gmail.com>
2019-11-04 01:14:08 +0530

    added support for the current year

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const makeCommit = n => {
   if(n===0) return simpleGit().push();
   const x = random.int(0, 54);
   const y = random.int(0,6);
-  const DATE = moment().subtract(2, 'y').add(1, 'd').add(x, 'w').add(y, 'd').format();
+  const DATE = moment().subtract(1, 'y').add(1, 'd').add(x, 'w').add(y, 'd').format();
   const data = {
     date: DATE
   }
